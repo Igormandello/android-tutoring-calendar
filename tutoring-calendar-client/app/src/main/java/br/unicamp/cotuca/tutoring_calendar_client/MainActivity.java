086052d8b72920463ace5efe4ebc473f83f0e48d
Igormandello <igormandello@gmail.com>
2018-04-11 11:31:02 -0300

    Creates Android client project

---
+++ tutoring-calendar-client/app/src/main/java/br/unicamp/cotuca/tutoring_calendar_client/MainActivity.java
@@ -0,0 +1,13 @@
+package br.unicamp.cotuca.tutoring_calendar_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
