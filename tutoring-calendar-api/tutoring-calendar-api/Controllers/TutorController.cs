b4f8f7080a7bb37ee9d2597a18b7982a7c3bfb8e
Igormandello <igormandello@gmail.com>
2018-04-19 11:34:31 -0300

    Adds tutor's image

--- tutoring-calendar-api/tutoring-calendar-api/Controllers/TutorController.cs
+++ tutoring-calendar-api/tutoring-calendar-api/Controllers/TutorController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Net.Http;
-using System.Web;
+using System.Net.Http.Formatting;
+using System.Reflection;
+using System.Resources;
 using System.Web.Http;
 using tutoring_calendar_api.DB.Repositories;
 using tutoring_calendar_api.Models;
@@ -26,6 +31,21 @@ namespace tutoring_calendar_api.Controllers
         public Tutor GetByRA(int ra)
         {
             return repo.GetByID(ra);
+        }
+
+        [Route("{ra}/image")]
+        [AcceptVerbs("GET")]
+        public String GetImage(int ra)
+        {
+            Image i = (Image)Properties.Resources.ResourceManager.GetObject(ra + "");
+            if (i == null)
+                i = Properties.Resources.no_image;
+
+            MemoryStream ms = new MemoryStream();
+            i.Save(ms, i.RawFormat);
+
+            byte[] array = ms.ToArray();
+            return Convert.ToBase64String(array);
         }
 
         [Route("")]
