9ad96839b2cccaa32f265b5b56ef774ebe662cc6
Igormandello <igormandello@gmail.com>
2018-04-27 08:05:15 -0300

    Removes the C# API
    - The new API will be implemented in node.js

--- tutoring-calendar-api/tutoring-calendar-api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace tutoring_calendar_api.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- tutoring-calendar-api/tutoring-calendar-api/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tutoring_calendar_api.WebApiApplication" Language="C#" %>
